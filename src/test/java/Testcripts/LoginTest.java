bb92d6670539994471a24c166de971003b705899
sainadh213 <sainadh213@gmail.com>
2024-10-04 15:55:08 +0530

    2

--- src/test/java/Testcripts/LoginTest.java
+++ src/test/java/Testcripts/LoginTest.java
@@ -12,8 +12,7 @@ import com.Tutorialsninja.Base.JavaUtil;
 import com.Tutorialsninja.ObjectRepo.LoginPage;
 
 @Listeners(com.Tutorialsninja.Base.Listeners.class)
-//Added on 03.10.2024
-//First comments on 04.10.2024
+
 public class LoginTest extends Base {
 	LoginPage lp;
 	FileUtil fu = new FileUtil();

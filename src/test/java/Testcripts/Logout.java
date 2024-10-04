18fa9c5bc909809de0b352281050d50acd3b4b19
sainadh213 <104199857+sainadh213@users.noreply.github.com>
2024-10-04 16:32:04 +0530

    Update Logout.java

--- src/test/java/Testcripts/Logout.java
+++ src/test/java/Testcripts/Logout.java
@@ -5,6 +5,7 @@ public class Logout {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is logout class");
+		System.out.println("Added");
 	}
 
 }
